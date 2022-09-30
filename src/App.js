02e2e6bab8d935102f2ab931acd356effc8c8a84
YeOldeEricC <eachapman.work@gmail.com>
2022-09-30 19:07:08 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 function App() {
-  return (
-    <div className='page'>
-      <p>ex</p>
-    </div>
-  );
+	return (
+		<div className='page'>
+			<div className='topbar'>
+				
+			</div>
+			<div className='contents'>
+				<span className='sidebar'>
+					
+				</span>
+				<span className='editor'>
+					<div>
+						<span className='linenums'>
+							
+						</span>
+						<span className='textinp'>
+							
+						</span>
+						<span className='overview'>
+							
+						</span>
+					</div>
+					<div className='statusbar'>
+						
+					</div>
+				</span>
+			</div>
+		</div>
+	);
 }
 
 export default App;
